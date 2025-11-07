e94d22afd1daf052e71ef1d749f57c5689449a59
Alexander Tarba <sachatarba@rambler.ru>
2025-11-08 00:48:01 +0300

    feat: Add automatic table creation on startup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from app.api.v1 import persons  # Импортируем наш роутер
+from app.api.v1 import persons
+
+from app.core.database import engine
+from app.models.person import Person
+
+Person.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="Persons API",
